3217f645141c18c8de3008c940a8ffc3595e8e3d
arka-alma <arka.karmakar@almabetter.com>
2024-06-25 20:07:17 +0530

    backend fixed

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const mongoURI = "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+// const mongoURI = "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+const mongoURI =
+  "mongodb+srv://arkasourav7:itsDb0@cluster0.gydpsdk.mongodb.net/iNotesDb?retryWrites=true&w=majority&appName=Cluster0";
 
-const connectToMongo = ()=>{
-    mongoose.connect(mongoURI, ()=>{
-        console.log("Connected to Mongo Successfully");
-    })
-}
+const connectToMongo = () => {
+  mongoose.connect(mongoURI, (err) => {
+    if (err) console.log("error:", err);
+    else console.log("Connected to Mongo Successfully");
+  });
+};
 
 module.exports = connectToMongo;
